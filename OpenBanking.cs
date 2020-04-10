4b74a688fe27f288fd343870e9040e2a686b8171
haren686 <59687497+haren686@users.noreply.github.com>
2020-04-10 19:23:55 +0000

    Add files via upload

---
+++ OpenBanking.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddItemsToDynamoDB
+{
+
+    public class Rootobject
+    {
+        public Class1[] Property1 { get; set; }
+    }
+
+    public class Class1
+    {
+        public string GUID { get; set; }
+        public string LoanType { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string CreditCardVarient { get; set; }
+        public string CurrentCardLimit { get; set; }
+        public string ServiceProvidingBank { get; set; }
+        public string CreditScore { get; set; }
+        public string RiskCatagory { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DocumentModel;
+using Newtonsoft.Json;
+
+namespace AddItemsToDynamoDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var client = new AmazonDynamoDBClient(Environment.GetEnvironmentVariable("ACCESS_KEY_ID"), Environment.GetEnvironmentVariable("SECRET_ACCESS_KEY"), RegionEndpoint.USWest2);
+            //var tables = client.GetItemAsync();
+            var table = Table.LoadTable(client, "OpenBanking_MI");
+            //Table table;
+            //var tblLoad = Table.TryLoadTable(client, "OPenBanking_MI", out table);
+            List<Class1> items;
+            using (StreamReader r = new StreamReader("C:/Users/harendra/source/repos/AddItemsToDynamoDB/OpenBanking.json"))
+            {
+                string json = r.ReadToEnd();
+                items = JsonConvert.DeserializeObject<List<Class1>>(json);
+
+                foreach (var itm in items)
+                {
+                    var record = Document.FromJson(JsonConvert.SerializeObject(itm));
+                    table.PutItemAsync(record);
+                }
+            }
+
+        }
+    }
+}
+// using Amazon.DynamoDBv2;
+// using Amazon.DynamoDBv2.DocumentModel;
+
